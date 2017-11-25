b1e645fb5370979d82133fde468b735bb7d7e4cd
James Burgess <jamesmillerburgess@gmail.com>
2017-11-25 07:55:13 +0100

    Render something

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../imports/components/App/App';
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
---
+++ imports/components/App/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Freight For All</div>;
+
+export default App;
